4d199fa9eda01dbb9c6564d642dc39e0c218b68d
jicadz <cadiz.joshuaiangit2011@gmail.com>
2023-10-02 21:13:11 +0800

    first-commit

---
+++ settings/settings.js
@@ -0,0 +1,50 @@
+const checkbox = document.getElementById("checkBox");
+const head = document.getElementById("head");
+const modes = document.getElementById("modes");
+
+const circle1 = document.getElementById("circle1");
+const circle2 = document.getElementById("circle2");
+
+const languageBtn = document.getElementById("language-btn");
+
+const buttons = document.getElementById("buttons");
+
+function darkMode() {
+    if (checkbox.checked) {
+        document.body.style.backgroundColor="#28282B";
+        // head
+        head.style.backgroundColor="#28282B";
+        head.querySelector("p").style.color="white";
+        head.querySelector("i").style.color="white";
+        modes.style.backgroundColor="#28282B";
+        // circle
+        circle1.querySelector("p").style.color="white";
+        circle2.querySelector("p").style.color="white";
+        // language-btn
+        languageBtn.style.borderColor="white";
+        languageBtn.style.backgroundColor="#28282B";
+        languageBtn.querySelector("p").style.color="white";
+        languageBtn.querySelector("i").style.color="white";
+        // buttons div
+        buttons.style.backgroundColor="#28282B";
+
+    } else {
+        document.body.style.backgroundColor="white";
+        // head
+        head.style.backgroundColor="#fcce22";
+        head.querySelector("p").style.color="#323660";
+        head.querySelector("i").style.color="#323660";
+        modes.style.backgroundColor="white";
+        // circle
+        circle1.querySelector("p").style.color="#323660";
+        circle2.querySelector("p").style.color="#323660";
+        // language-btn
+        languageBtn.style.borderColor="#323660";
+        languageBtn.style.backgroundColor="white";
+        languageBtn.querySelector("p").style.color="#323660";
+        languageBtn.querySelector("i").style.color="#323660";
+        // buttons div
+        buttons.style.backgroundColor="white";
+    }
+}
+
